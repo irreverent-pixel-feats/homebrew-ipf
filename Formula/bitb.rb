ac48d4446a6800cbf254693f793d9ab4207b1efd
Dom De Re <domdere@domdere.com>
2018-03-30 16:25:10 +1100

    bitb
    - push a new update

--- Formula/bitb.rb
+++ Formula/bitb.rb
@@ -1,13 +1,15 @@
 class Bitb < Formula
   desc "Bitbucket CLI"
   homepage "https://github.com/irreverent-pixel-feats/bitbucket"
-  url "https://dl.bintray.com/irreverent-pixel-feats/brew/bottles/bitb-0.0.1-20180330155346-c526900.bottle.tar.gz"
-  version "0.0.1-20180330155346-c526900"
-  sha256 "6c2e3ccb8fc613c6b5cf84ae34370be4cdb0342007631c749eaaac977dcbd83c"
+  url "https://dl.bintray.com/irreverent-pixel-feats/brew/bottles/bitb-0.0.1-20180330162209-a70044e.bottle.tar.gz"
+  version "0.0.1-20180330162209-a70044e"
+  sha256 "00e4f1e87e6b5fad3b5d11b0b2bc18bfa7306b3a3f00fc1e117cdb9ac0b1c656"
 
   def install
     bin.install "bin/bitb"
-    prefix.install Dir["_completions"]
+    bash_completion.install "_completions/bitb.bash"
+    zsh_completion.install "_completions/zsh/_bitb"
+    fish_completion.install "_completions/bitb.fish"
   end
 
   def test
